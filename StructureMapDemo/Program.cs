513fe4812cfc17c874f5502457d9925ff8b6437b
Aryan <Aryan@DESKTOP-E1QNBHS>
2018-03-19 23:12:25 +0530

    Add project files.

---
+++ StructureMapDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StructureMap;
+
+namespace StructureMapDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new Container(_ =>
+            {
+                _.For<DummyClass>().Use<DummyClass>();
+                
+            });
+
+            DummyClass dummyClass = container.GetInstance<DummyClass>();
+
+            dummyClass.Display();
+        }
+    }
+
+    public class DummyClass
+    {
+        public void Display()
+        {
+            Console.WriteLine("Display my data !!");
+        }
+    }
+ 
+
+}
